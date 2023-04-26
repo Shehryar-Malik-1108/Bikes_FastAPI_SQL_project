7cfaecafa12cc0aa4795f19abb59f69b0459bb4f
Shehryar-Malik-1108 <shehryarmalik1108@gmail.com>
2023-04-26 17:17:48 +0500

    first commit

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from pydantic import BaseModel
 from typing import Optional, List
 from my_database import MyDatabase
 
-
 app = FastAPI(title="Shehryar Ke Bikes")
 db = MyDatabase()
 
@@ -16,9 +15,10 @@ def home():
 @app.get("/Select_Bike")
 def select_bike(id: int):
     bike = db.select_bike(id)
-    if "message" in bike:
-        raise HTTPException(status_code=404, detail=bike["message"])
-    return bike
+    if bike:
+        return bike
+    else:
+        return f"Bike: {id} does not exist!"
 
 
 @app.get("/All_Bikes")
@@ -29,46 +29,52 @@ def get_all_bikes():
 @app.post("/Insert_bike")
 def insert_bike(id: int, name: str, cc: int, color: str, price: int):
     result = db.insert_bike(id, name, cc, color, price)
-    if "message" in result:
-        raise HTTPException(status_code=400, detail=result["message"])
-    return {"message": "Bike created successfully."}
+    if bike in result:
+        return f"bike:{name} created successfully."
+    else:
+        return f"{name} Not Created."
 
 
 @app.post("/Insert_many_bikes")
 def insert_many_bike(bikes: list[dict]):
     result = db.insert_many_bike([bike for bike in bikes])
-    if "message" in result:
-        raise HTTPException(status_code=400, detail=result["message"])
-    return {"message": "Bikes created successfully."}
+    if bike1 in result:
+        return {"message": "Bikes created successfully."}
+    else:
+        return f"Bikes not created."
 
 
 @app.put("/Update_bike")
 def update_bike(id: int, price: int):
     result = db.update_bike(id, price)
-    if "message" in result:
-        raise HTTPException(status_code=404, detail=result["message"])
-    return {"message": "Bike updated successfully."}
+    if bike in result:
+        return f"{id} updated successfully."
+    else:
+        return f"{id} not deleted."
 
 
 @app.put("/Update_many_bikes")
 def update_many_bikes(bikes: list[dict]):
     result = db.update_many_bikes([bike for bike in bikes])
-    if "message" in result:
-        raise HTTPException(status_code=404, detail=result["message"])
-    return {"message": "Bikes updated successfully."}
+    if bike1 in result:
+        return "Bikes updated successfully."
+    else:
+        return "Bikes not updated."
 
 
 @app.delete("/Delete_bike")
 def delete_bike(id: int):
     result = db.delete_bike(id)
-    if "message" in result:
-        raise HTTPException(status_code=404, detail=result["message"])
-    return {"message": "Bike deleted successfully."}
+    if bike in result:
+        return f"{id} deleted successfully."
+    else:
+        return f"{id} not deleted"
 
 
 @app.delete("/Delete_many_bikes")
 def delete_many_bikes(bikes: list[int]):
     result = db.delete_many_bikes(bikes)
-    if "message" in result:
-        raise HTTPException(status_code=404, detail=result["message"])
-    return {"message": "Bikes deleted successfully."}
+    if bike in result:
+        return "Bikes deleted successfully."
+    else:
+        return "Bikes not deleted."
--- my_database.py
+++ my_database.py
@@ -50,34 +50,34 @@ class MyDatabase:
             })
         return {"bikes": bikes}
 
-    def insert_bike(self, id: int, name: str, cc: int, color: str, price: int, bikes=None):
+    def insert_bike(self, id: int, name: str, cc: int, color: str, price: int):
         cursor = self.connection.cursor()
-        if bikes is None:
-            cursor.execute("INSERT INTO bikes (id, name, cc, color, price) VALUES (?, ?, ?, ?, ?)", (id, name, cc, color, price))
+        try:
+            cursor.execute("INSERT INTO bikes (id, name, cc, color, price) VALUES (?, ?, ?, ?, ?)",
+                           (id, name, cc, color, price))
             self.connection.commit()
-            return {"message": f"Bike {name} created"}
-        else:
-
-            cursor.executemany("INSERT INTO bikes (id, name, cc, color, price) VALUES (?, ?, ?, ?, ?)",
-                            [(bike["id"], bike["name"], bike["cc"], bike["color"], bike["price"]) for bike in bikes])
-            self.connection.commit()
-            return {"message": f"{len(bikes)} bikes created"}
-        return {"message": "Error occurred while creating new bike(s)."}
+            return {"message": f"Bike {name} created successfully"}
+        except Exception as e:
+            return f"Error occurred while creating new bike due to: {e}"
 
     def insert_many_bike(self, bikes: list[dict]):
         cursor = self.connection.cursor()
         cursor.executemany("INSERT INTO bikes (id, name, cc, color, price) VALUES (?, ?, ?, ?, ?)",
-                        [(bike["id"], bike["name"], bike["cc"], bike["color"], bike["price"]) for bike in bikes])
+                           [(bike["id"], bike["name"], bike["cc"], bike["color"], bike["price"]) for bike in bikes])
         self.connection.commit()
-        return {"message": f"{len(bikes)} bikes created"}
+        if bike1:
+            return f"{len(bikes)} bikes created"
+        else:
+            return "Bikes not created."
 
     def update_bike(self, id: int, price: int):
         cursor = self.connection.cursor()
         cursor.execute("UPDATE bikes SET price=? WHERE id=?", (price, id))
         self.connection.commit()
         if cursor.rowcount == 1:
-            return {"message": f"Bike updated."}
-        return {"message": f"Error occurred while updating bike."}
+            return f"{id} updated."
+        else:
+            return f"Error occurred while updating {id}."
 
     def update_many_bikes(self, bikes: list[dict]):
         cursor = self.connection.cursor()
@@ -85,16 +85,18 @@ class MyDatabase:
             cursor.execute(f"UPDATE bikes SET name="f"price='{bike['price']}' WHERE id='{bike['id']}'")
         self.connection.commit()
         if cursor.rowcount > 0:
-            return {"message": f"{cursor.rowcount} bike(s) updated."}
-        return {"message": "No bikes updated."}
+            return f"{cursor.rowcount} bike(s) updated."
+        else:
+            return "No bikes updated."
 
     def delete_bike(self, id: int):
         cursor = self.connection.cursor()
         cursor.execute(f"DELETE FROM bikes WHERE id={id}")
         self.connection.commit()
         if cursor.rowcount == 1:
-            return {"message": "Bike deleted."}
-        return {"message": "Error occurred while deleting bike."}
+            return f"{id} deleted."
+        else:
+            return "Error occurred while deleting bike."
 
     def delete_many_bikes(self, bikes: list[int]):
         cursor = self.connection.cursor()
@@ -102,8 +104,8 @@ class MyDatabase:
         cursor.execute(f"DELETE FROM bikes WHERE id IN ({id_list})")
         self.connection.commit()
         if cursor.rowcount > 0:
-            return {"message": f"{cursor.rowcount} bike(s) deleted."}
-        return {"message": "No bikes deleted."}
+            return f"{cursor.rowcount} bike(s) deleted."
+        return "Bikes not deleted."
 
 
 if __name__ == "__main__":
